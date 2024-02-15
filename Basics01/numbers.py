8bb60005181a1598bb5333ab365aa9fd45228d37
0007ishav <2002ishavs@gmail.com>
2024-02-15 19:17:39 +0530

    Numbers

---
+++ Basics01/numbers.py
@@ -0,0 +1,18 @@
+# repr() provides a detailed and unambiguous representation of an object, often used for debugging and development.
+# str() provides a human-readable representation of an object.
+# print() is a built-in function for displaying text or object representations on the console. It implicitly uses str() on its arguments.
+
+# math.floor() will return the floor value.
+# math.trunc() will return value closest to zero.
+
+# 0o20 = o for octa
+# 0xFF = x for hexa
+# 0b100 = b for binary
+
+# Converting numbers
+# oct(16)
+# hex(64)
+# bin(64)
+# int('64',8 )  - octa
+# int('64', 16 ) - hexa
+# int('64', 2 ) - binary
