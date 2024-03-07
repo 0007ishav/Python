a7a0ef7a0e2cd770edcaa6b30e0dec10ba592bc7
0007ishav <2002ishavs@gmail.com>
2024-03-07 23:55:32 +0530

    BTS of loops

---
+++ IterationTools/iterators.py
@@ -0,0 +1,14 @@
+username = "strnge"
+print(username)
+
+# The iter() function in Python returns an iterator object for the given argument.
+# An iterator is an object that can be iterated over, meaning that you can traverse through all the values.
+
+
+
+# Run this in terminal
+# D = {'a':1, 'b':2}
+# I = iter(D)
+
+# print the I it will give this result.
+# <dict_keyiterator object at 0x0000020361736ED0>   
