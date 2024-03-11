ba28a341421c4396fc908f8f19d3371f12f7deb9
0007ishav <2002ishavs@gmail.com>
2024-03-11 23:40:44 +0530

    Solved

--- Oops/Solution.py
+++ Oops/Solution.py
@@ -55,10 +55,43 @@
 
 # Solution 5
 
+# class Car:
+#     def __init__(self, brand, model):
+#         self.__brand = brand
+#         self.model = model
+
+#     def get_brand(self):
+#         return self.__brand + " !"
+
+#     def full_name(self):
+#         return f"{self.__brand} {self.model}"
+    
+#     def fuel_type(self):
+#         return "Petrol or Diesel"
+
+# class ElectriCar(Car):
+#     def __init__(self, brand, model, battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size = battery_size
+
+#     def fuel_type(self):
+#         return "Electric Charge"
+    
+# my_tesla = ElectriCar("Tesla", "Model S", "85kWh")
+# print(my_tesla.fuel_type())
+
+# safari = Car("Tata", "Safari")
+# print(safari.fuel_type())
+
+# Solution 6
+
 class Car:
+    total_car = 0
     def __init__(self, brand, model):
         self.__brand = brand
         self.model = model
+        Car.total_car += 1
+
 
     def get_brand(self):
         return self.__brand + " !"
@@ -68,17 +101,14 @@ class Car:
     
     def fuel_type(self):
         return "Petrol or Diesel"
-
-class ElectriCar(Car):
-    def __init__(self, brand, model, battery_size):
-        super().__init__(brand, model)
-        self.battery_size = battery_size
-
-    def fuel_type(self):
-        return "Electric Charge"
     
-my_tesla = ElectriCar("Tesla", "Model S", "85kWh")
-print(my_tesla.fuel_type())
-
-safari = Car("Tata", "Safari")
-print(safari.fuel_type())
+# safari = Car("Tata", "Safari")
+# print(safari.total_car)
+    
+# These two are not okay to get access of total car variable.
+    
+# test = Car("test", "test")
+# print(test.total_car)
+    
+print(Car.total_car)   # we can directly access the variables.
+# Sometimes garbage collector dont clean immediately for optimisation purpose.
