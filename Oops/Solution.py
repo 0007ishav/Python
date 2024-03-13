8cb86eee98119f4cb2cdf5b109075bd99c80af21
0007ishav <2002ishavs@gmail.com>
2024-03-13 22:58:49 +0530

    Solved

--- Oops/Solution.py
+++ Oops/Solution.py
@@ -115,11 +115,117 @@
 
 # Solution 7
 
+# class Car:
+#     total_car = 0
+#     def __init__(self, brand, model):
+#         self.__brand = brand
+#         self.model = model
+#         Car.total_car += 1
+
+
+#     def get_brand(self):
+#         return self.__brand + " !"
+
+#     def full_name(self):
+#         return f"{self.__brand} {self.model}"
+    
+#     def fuel_type(self):
+#         return "Petrol or Diesel"
+    
+#     @staticmethod
+#     def general_description():
+#         return "Cars are means of transport."
+    
+# my_car = Car("Tata", "Safari")
+# Car("Tata", "Nexon")
+# # print(my_car.general_description())
+# print(Car.general_description())
+
+# # Solution 8
+
+# class Car:
+#     total_car = 0
+#     def __init__(self, brand, model):
+#         self.__brand = brand
+#         self.__model = model
+#         Car.total_car += 1
+
+
+#     def get_brand(self):
+#         return self.__brand + " !"
+
+#     def full_name(self):
+#         return f"{self.__brand} {self.__model}"
+    
+#     def fuel_type(self):
+#         return "Petrol or Diesel"
+    
+#     @staticmethod
+#     def general_description():
+#         return "Cars are means of transport."
+    
+#     @property    # Set the model to read only.
+#     def model(self):
+#          return self.__model
+
+# my_car = Car("Tata", "Safari")
+# Car("Tata", "Nexon")
+
+# # print(my_car.general_description())
+# print(Car.general_description())
+
+# # my_car.model = "City"
+# print(my_car.model)
+
+
+# # Solution 9
+
+# class Car:
+#     total_car = 0
+#     def __init__(self, brand, model):
+#         self.__brand = brand
+#         self.__model = model
+#         Car.total_car += 1
+
+
+#     def get_brand(self):
+#         return self.__brand + " !"
+
+#     def full_name(self):
+#         return f"{self.__brand} {self.__model}"
+    
+#     def fuel_type(self):
+#         return "Petrol or Diesel"
+    
+#     @staticmethod
+#     def general_description():
+#         return "Cars are means of transport."
+    
+#     @property    # Set the model to read only.
+#     def model(self):
+#          return self.__model
+
+# class ElectriCar(Car):
+#     def __init__(self, brand, model, battery_size):
+#         super().__init__(brand, model)
+#         self.battery_size = battery_size
+
+#     def fuel_type(self):
+#         return "Electric Charge"
+    
+# my_tesla = ElectriCar("Tesla", "Model S", "85kWh")
+
+# print(isinstance(my_tesla, Car))
+# print(isinstance(my_tesla, ElectriCar))
+
+
+# Solution 10
+
 class Car:
     total_car = 0
     def __init__(self, brand, model):
         self.__brand = brand
-        self.model = model
+        self.__model = model
         Car.total_car += 1
 
 
@@ -127,7 +233,7 @@ class Car:
         return self.__brand + " !"
 
     def full_name(self):
-        return f"{self.__brand} {self.model}"
+        return f"{self.__brand} {self.__model}"
     
     def fuel_type(self):
         return "Petrol or Diesel"
@@ -136,7 +242,33 @@ class Car:
     def general_description():
         return "Cars are means of transport."
     
-my_car = Car("Tata", "Safari")
+    @property    # Set the model to read only.
+    def model(self):
+         return self.__model
 
-# print(my_car.general_description())
-print(Car.general_description())
+class ElectriCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+    def fuel_type(self):
+        return "Electric Charge"
+    
+my_tesla = ElectriCar("Tesla", "Model S", "85kWh")
+
+# Pass is special keyword used as a placeholder for a code. In python when we make a class and we want to specify the use case later we just use pass to avoid error and code still being empty
+
+class Battery:
+    def battery_info(self):
+        return "This is a battery."
+
+class Engine:
+    def engine_info(self):
+        return "This is an engine."
+
+class ElectricCarTwo(Battery, Engine, Car):
+    pass
+
+my_new_tesla = ElectricCarTwo("Tesla", "Model S")
+print(my_new_tesla.engine_info())
+print(my_new_tesla.battery_info())
